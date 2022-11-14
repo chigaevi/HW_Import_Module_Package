ab59c7b8368f17f23a12a74042a6a6ac74466050
chigaevi <chigaev.i@gmail.com>
2022-11-14 15:58:21 +0700

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,2 @@
+def get_employees():
+    return print('def get_employees started')
---
+++ main.py
@@ -0,0 +1,14 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import datetime
+
+
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    date = datetime.date.today()
+    print(date)
+
+
+
